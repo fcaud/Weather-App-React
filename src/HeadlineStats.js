e29edb285f6877c3da5bfcacc5629f15375c2122
fcaud <78268012+fcaud@users.noreply.github.com>
2021-05-17 17:56:30 +0100

    Transfer api call into module component

--- src/HeadlineStats.js
+++ src/HeadlineStats.js
@@ -1,54 +1,18 @@
-// move axios to module
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./HeadlineStats.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as Icons from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
 
 export default function HeadlineStats(props) {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
-
-  function runAPI() {
-    const apiKey = "a853abb2375faaf0d512fcc19dee1229";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(getWeather);
-  }
-
-  useEffect(() => {
-    runAPI();
-  }, []);
-
-  function getWeather(response) {
-    console.log(response.data);
-    const data = response.data;
-    const timestamp = new Date(data.dt * 1000);
-    setWeatherData({
-      city: data.name,
-      temp: Math.round(data.main.temp),
-      wind: Math.round(data.wind.speed),
-      humidity: Math.round(data.main.humidity),
-      description: data.weather[0].main,
-      fullDescription: data.weather[0].description,
-      timeStamp: timestamp,
-    });
-
-    props.updateDate(timestamp);
-
-    setReady(true);
-  }
-
-  if (ready) {
+  if (props.ready) {
     return (
       <div className="HeadlineStats">
-        <h2 className="city-header">{weatherData.city}</h2>
+        <h2 className="city-header">{props.weatherData.city}</h2>
         <div className="headline-weather-icon icon">
           <FontAwesomeIcon icon={Icons.faCloudSun} />
         </div>
         <div className="headline-temperature">
-          <span className="temp-num">{weatherData.temp}</span>°C
+          <span className="temp-num">{props.weatherData.temp}</span>°C
         </div>
         <div className="row extra-stats">
           <div className="col"></div>
@@ -56,14 +20,14 @@ export default function HeadlineStats(props) {
             <p className="p-wind">
               Wind speed:
               <br />
-              <span>{weatherData.wind}</span>kmh
+              <span>{props.weatherData.wind}</span>kmh
             </p>
           </div>
           <div className="col">
             <p className="p-humidity">
               Humidity:
               <br />
-              <span>{weatherData.humidity}</span>%
+              <span>{props.weatherData.humidity}</span>%
             </p>
           </div>
           <div className="col"></div>
--- src/Module.js
+++ src/Module.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Module.css";
 import Forecast from "./Forecast";
 import HeadlineStats from "./HeadlineStats";
 import Placeholder from "./Placeholder";
 import Search from "./Search";
+import axios from "axios";
 
 export default function Module(props) {
+  const [weatherData, setWeatherData] = useState({});
+  const [ready, setReady] = useState(false);
+
+  function runAPI() {
+    const apiKey = "a853abb2375faaf0d512fcc19dee1229";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getWeather);
+  }
+
+  useEffect(() => {
+    runAPI();
+  }, []);
+
+  function getWeather(response) {
+    console.log(response.data);
+    const data = response.data;
+    const timestamp = new Date(data.dt * 1000);
+    setWeatherData({
+      city: data.name,
+      temp: Math.round(data.main.temp),
+      wind: Math.round(data.wind.speed),
+      humidity: Math.round(data.main.humidity),
+      description: data.weather[0].main,
+      fullDescription: data.weather[0].description,
+      timeStamp: timestamp,
+    });
+    props.updateDate(timestamp);
+
+    setReady(true);
+  }
+
   let tempStatus = props.tempStatus;
   if (props.moduleID === "liveModule") {
     return (
@@ -15,6 +48,8 @@ export default function Module(props) {
           city={props.city}
           temp={props.temp}
           updateDate={props.updateDate}
+          weatherData={weatherData}
+          ready={ready}
         />
         <Forecast />
       </section>
