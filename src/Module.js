6c1a7bc5c621d747d150c8a23464facd3878f931
fcaud <78268012+fcaud@users.noreply.github.com>
2021-06-01 17:51:23 +0100

    disable react hook warning

--- src/Module.js
+++ src/Module.js
@@ -32,6 +32,7 @@ export default function Module(props) {
 
   useEffect(() => {
     runAPI(props.city, "metric");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function getWeather(response) {
